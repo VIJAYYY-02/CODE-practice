7c0d83a52fdff0c48b61cdec749da1e41339a10e
VJ <vijaysinghrajput.020207@gmail.com>
2026-08-28 18:48:34 +0530

    Add October Marathon (300)

---
+++ codechef/300/OCTATHON - October Marathon.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+    int X;cin>>X;
+    if(X<3) cout<<"GOLD"<<endl;
+    else if(X<6) cout<<"SILVER"<<endl;
+    else cout<<"BRONZE"<<endl;
+
+
+
+}
