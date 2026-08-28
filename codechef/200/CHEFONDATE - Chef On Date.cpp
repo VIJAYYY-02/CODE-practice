fcd8523b3e265f3f1e6ab76aaa5254cc513e98fa
VJ <vijaysinghrajput.020207@gmail.com>
2026-08-28 12:35:08 +0530

    Add Chef On Date (200)

---
+++ codechef/200/CHEFONDATE - Chef On Date.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+  int t;
+  cin>>t;
+  while(t--){
+      int x,u;
+      cin>>x>>u;
+      if(x>=u) cout<<"yes"<<endl;
+      else cout<<"no"<<endl;
+  }
+}
