b38ae02135f9466a2b514d315af3683c72e2fecf
VJ <vijaysinghrajput.020207@gmail.com>
2026-08-28 19:09:05 +0530

    Add MATH1 Enrolment (300)

---
+++ codechef/300/M1ENROL - MATH1 Enrolment.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int X, Y;
+        cin >> X >> Y;
+        if (Y > X)
+            cout << Y - X << endl;
+        else
+            cout << 0 << endl;
+    }
+    return 0;
+}
