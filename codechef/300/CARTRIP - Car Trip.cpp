5eb19c1e2d3157248fedbfc793f249931e2f1811
VJ <vijaysinghrajput.020207@gmail.com>
2026-08-28 19:13:32 +0530

    Add Car Trip (300)

---
+++ codechef/300/CARTRIP - Car Trip.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int X;
+        cin >> X;
+        if (X < 300)
+            cout << 300 * 10 << endl;
+        else
+            cout << X * 10 << endl;
+    }
+    return 0;
+}
