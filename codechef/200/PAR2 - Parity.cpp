4e219e487e9b531a1918e43432edfab1fe25fed7
VJ <vijaysinghrajput.020207@gmail.com>
2026-08-28 18:14:37 +0530

    Add Parity (200)

---
+++ codechef/200/PAR2 - Parity.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int y;
+	    cin>>y;
+	    if((y%2)==0)
+	      cout<<"yes"<<endl;
+	    else
+	     cout<<"no"<<endl;
+	}
+
+}
