2ebca357904368d1f4a5ca568f8ad46049d26027
VJ <vijaysinghrajput.020207@gmail.com>
2026-08-28 19:10:35 +0530

    Add Lunchtime (300)

---
+++ codechef/300/LTIME - Lunchtime.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int X;
+        cin >> X;
+        if (X >= 1 && X <= 4)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+    return 0;
+}
