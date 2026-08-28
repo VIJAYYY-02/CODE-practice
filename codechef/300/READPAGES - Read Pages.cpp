cbced8be997cd21282565441bd85fb5ed4eab6b6
VJ <vijaysinghrajput.020207@gmail.com>
2026-08-28 19:07:00 +0530

    Add Read Pages (300)

---
+++ codechef/300/READPAGES - Read Pages.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int N, X, Y;
+        cin >> N >> X >> Y;
+        if (X * Y >= N)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+    return 0;
+}
