6ba4f488f65af6f97e6e1d9862906b887ce0ebe8
VJ <vijaysinghrajput.020207@gmail.com>
2026-08-28 18:49:13 +0530

    Add Just One More Episode (300)

---
+++ codechef/300/ONEMORE - Just One More Episode.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int T; cin >> T;
+while (T--) {
+    int X; cin >> X;
+    cout << (X > 24 ? "YES" : "NO") << endl;
+}
+
+}
