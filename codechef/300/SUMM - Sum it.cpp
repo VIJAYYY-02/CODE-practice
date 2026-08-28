c029836f2583635ac680cba9d16e0f5c2f9aecb7
VJ <vijaysinghrajput.020207@gmail.com>
2026-08-28 18:42:02 +0530

    Add Sum it (300)

---
+++ codechef/300/SUMM - Sum it.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+ int T;cin>>T;
+while(T--){
+    int A,B,C;cin>>A>>B>>C;
+    cout<<((A+B==C)?"YES":"NO")<<endl;
+}
+
+}
