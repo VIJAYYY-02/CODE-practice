f89a4c0a34357eb9c9c5d4e393cb646263057cf9
VJ <vijaysinghrajput.020207@gmail.com>
2026-08-29 17:37:58 +0530

    Add Is it hot or cold (400)

---
+++ codechef/400/HOTCOLD - Is it hot or cold.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int C;
+        cin >> C;
+        if (C > 20)
+            cout << "HOT" << endl;
+        else
+            cout << "COLD" << endl;
+    }
+    return 0;
+}
