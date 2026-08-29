8f84e58e052ed86cdf7f47db82dca01920d01453
VJ <vijaysinghrajput.020207@gmail.com>
2026-08-29 14:01:03 +0530

    Add True and False Paper (300)

---
+++ codechef/300/TFPAPER - True and False Paper.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int N, K;
+        cin >> N >> K;
+        cout << N - K << endl;
+    }
+    return 0;
+}
