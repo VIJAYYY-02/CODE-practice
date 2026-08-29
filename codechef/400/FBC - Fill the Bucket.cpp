3c7672ed322d055283f7b27f6004e05b7d592e24
VJ <vijaysinghrajput.020207@gmail.com>
2026-08-29 17:38:52 +0530

    Add Fill the Bucket (400)

---
+++ codechef/400/FBC - Fill the Bucket.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int K, X;
+        cin >> K >> X;
+        cout << K - X << endl;
+    }
+    return 0;
+}
