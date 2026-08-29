b71615e4e7f7c6eae91699fc2bc577f92d276dbc
VJ <vijaysinghrajput.020207@gmail.com>
2026-08-29 13:57:43 +0530

    Add Six Friends (300)

---
+++ codechef/300/SIXFRIENDS - Six Friends.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int X, Y;
+        cin >> X >> Y;
+        cout << min(3 * X, 2 * Y) << endl;
+    }
+    return 0;
+}
